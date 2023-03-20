b34d8f8dd2aa3e0922c9d2855ad30c445cffc342
高莹豪 <yinghaogao097@gmail.com>
2023-03-20 10:15:28 +0800

    SpringSecurity引入依赖 项目启动

---
+++ springsecurity-quickstart/src/main/java/com/sangeng/SecurityApplication.java
@@ -0,0 +1,14 @@
+package com.sangeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author：Achen
+ */
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
